96e33e4679d425f426f0360c0c9344d29db779b5
26simran <103754435+26simran@users.noreply.github.com>
2024-02-14 17:30:37 +0000

    Variables

---
+++ basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 2385034
+let accountEmail = "simranpradhan2727@gmail.com"
+var accountPassword = "67483"
+accountCity = "Balasore"
+let accountState;
+
+// accountId = 3 // not allowed
+
+accountEmail = "hv@uh.com"
+accountPassword = "5878459"
+accountCity = "Warangal"
+
+
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+ 
+ /*
+   prefer not to use var
+   because of issue in block scope and function scope
+ */
+  
+
+console.log(accountId);
