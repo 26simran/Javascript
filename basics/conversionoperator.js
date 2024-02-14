236428d2cfca21662dfb3a3335d75c1a9ca8613f
26simran <103754435+26simran@users.noreply.github.com>
2024-02-14 18:17:46 +0000

    Conversion of datatypes

---
+++ basics/conversionoperator.js
@@ -0,0 +1,34 @@
+//let score = "56vjbg"
+
+//console.log (typeof score);
+//console.log (typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log ( valueInNumber); //Notanumber
+//console.log (typeof valueInNumber)
+
+// let score = null // 0
+// let score = undefined // Nan
+// let score = boolean // 1
+// let score = "Simi" // Nan
+
+// "53" => 53
+// "56ab" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = 1 // true 
+//let isLoggedIn = "" // false
+//let isLoggedIn = "Simran" //true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "simran" => true
+
+let someNumber = 89
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
